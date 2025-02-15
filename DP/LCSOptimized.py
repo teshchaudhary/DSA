ea667d65c10a2203bad976b06bfc5f5c1371288f
Tesh Chaudhary <chaudharytesh@gmail.com>
2025-02-15 17:20:28 +0530

    Added in DP: -> Space Optimised LCS -> Coin Changes

---
+++ DP/LCSOptimized.py
@@ -0,0 +1,18 @@
+def lcs_tab(s1, s2, n, m):
+    prev = [0 for _ in range(m+1)]
+    curr = [0 for _ in range(m+1)]
+    
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+                        
+            if s1[i-1] == s2[j-1]:
+                curr[j] = 1 + prev[j-1]
+            
+            else:
+                curr[j] = max(prev[j], curr[j-1])
+        
+        prev = curr
+
+    return prev[m]
+
+print(lcs_tab("XYZAB", "XYZA", 5, 4))
