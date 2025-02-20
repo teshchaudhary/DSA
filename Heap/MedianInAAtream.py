e3eff452738bbd8b6500ebfefc057cf9e0a02857
teshchaudhary <chaudharytesh@gmail.com>
2025-02-20 12:38:54 +0530

    Added explanation for Median In A Stream

--- Heap/MedianInAAtream.py
+++ Heap/MedianInAAtream.py
@@ -1,24 +1,28 @@
 import heapq
 
 def getMedian(arr):
-    minHeap = []
-    maxHeap = []
+    minHeap = []  # Min-heap for the larger half of the elements
+    maxHeap = []  # Max-heap for the smaller half (using negative values)
     res = []
+    
     for i in range(len(arr)):
+        # Step 1: Insert the element into the appropriate heap
         if not maxHeap or arr[i] <= -maxHeap[0]:
             heapq.heappush(maxHeap, -arr[i])
         else:
             heapq.heappush(minHeap, arr[i])
         
-        if len(maxHeap) > len(minHeap)+1:
+        # Step 2: Balance the heaps if necessary
+        if len(maxHeap) > len(minHeap) + 1:
             heapq.heappush(minHeap, -heapq.heappop(maxHeap))
-        elif len(maxHeap) < len(minHeap):
+        elif len(minHeap) > len(maxHeap):
             heapq.heappush(maxHeap, -heapq.heappop(minHeap))
-            
-        if i % 2 == 0:
-            res.append(-maxHeap[0]/1)
         
+        # Step 3: Calculate the median
+        if len(maxHeap) > len(minHeap):
+            res.append(-maxHeap[0])  # Odd number of elements
         else:
-            res.append((-maxHeap[0] + minHeap[0])/2)
+            # Even number of elements
+            res.append((-maxHeap[0] + minHeap[0]) / 2)
     
     return res
