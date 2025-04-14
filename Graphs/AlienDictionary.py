37373e6bb66139dfe4da25bf942514eb247afb22
teshchaudhary <teshchaudhary>
2025-04-14 10:44:19 +0530

    added in Graphs

--- Graphs/AlienDictionary.py
+++ Graphs/AlienDictionary.py
@@ -1,4 +1,3 @@
-#User function Template for python3
 from collections import defaultdict, deque
 
 class Solution:
@@ -41,64 +40,4 @@ class Solution:
         if len(order) != len(indegrees):
             return ""
             
-        return "".join(order)
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-import sys
-from collections import deque
-
-#Position this line where user code will be pasted.
-
-
-def validate(original, order):
-    char_map = {}
-    for word in original:
-        for ch in word:
-            char_map[ch] = 1
-
-    for ch in order:
-        if ch not in char_map:
-            return False
-        del char_map[ch]
-
-    if char_map:
-        return False
-
-    char_index = {ch: i for i, ch in enumerate(order)}
-
-    for i in range(len(original) - 1):
-        a, b = original[i], original[i + 1]
-        k, n, m = 0, len(a), len(b)
-        while k < n and k < m and a[k] == b[k]:
-            k += 1
-        if k < n and k < m and char_index[a[k]] > char_index[b[k]]:
-            return False
-        if k != n and k == m:
-            return False
-
-    return True
-
-
-if __name__ == "__main__":
-    input_data = sys.stdin.read().strip().split("\n")
-    index = 0
-    t = int(input_data[index])
-    index += 1
-    while t > 0:
-        words = input_data[index].split()
-        index += 1
-        original = words[:]
-
-        order = Solution.findOrder(words)
-
-        if order == "":
-            print("\"\"")
-        else:
-            print("true" if validate(original, order) else "false")
-        print("~")
-        t -= 1
-
-# } Driver Code Ends
+        return order
