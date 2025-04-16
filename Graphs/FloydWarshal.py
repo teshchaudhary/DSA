68bd75559a25b05ead62c5251be66173449c568d
Tesh Chaudhary <chaudharytesh@gmail.com>
2025-04-16 06:51:02 +0530

    Correction

--- Graphs/FloydWarshal.py
+++ Graphs/FloydWarshal.py
@@ -6,10 +6,9 @@ class Solution:
     def floydWarshall(self, dist):
         n = len(dist)
         INF = 10**8
-
+    
         for k in range(n):
             for i in range(n):
                 for j in range(n):
-                    
-                    if dist[i][k] != INF and dist[k][j] != INF:
-                        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+                    if dist[i][j] > dist[i][k] + dist[k][j]:
+                        dist[i][j] = dist[i][k] + dist[k][j]
