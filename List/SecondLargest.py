d74ec3342074280958ce2c46419ecbaa00dd8e77
Tesh Chaudhary <chaudharytesh@gmail.com>
2025-05-02 12:30:08 +0530

    refract

--- List/SecondLargest.py
+++ List/SecondLargest.py
@@ -1,22 +1,16 @@
-def secondLargest(l):
-    if len(l) <= 1:
-        return None
-
-    largest = l[0]
-    sLargest = None
-
-    for i in l[1:]:
-        if i > largest:
-            sLargest = largest
-            largest = i
-
-        elif i != largest:  # handles the duplicate values
-            # case for none can be [2,2,1]
-            # As Second largest was not set until index 1
-            if sLargest == None or sLargest < i:
-                sLargest = i
-
-    return (sLargest, largest)
-
-
-print(secondLargest([5, 12, 15, 20, 20]))
+class Solution:
+    def getSecondLargest(self, arr):
+        largest = float('-inf')
+        sLargest = float('-inf')
+        
+        for i in range(len(arr)):
+                    
+            if largest < arr[i]:
+                sLargest = largest
+                largest = arr[i]
+            
+            if arr[i] < largest:
+                sLargest = max(sLargest, arr[i])
+            
+        
+        return sLargest if sLargest != float('-inf') else -1
